76149f9e6585a60764393b19a475225b5e47c045
Karalito <karl.pigaga@gmail.com>
2021-10-22 17:01:08 +0300

    Added react router
    Added react-router-dom, react-router-bootstrap and made some improvements on frontend styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Nav, Navbar, Container, Row } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 function Header() {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Amplo</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Amplo</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav
@@ -13,8 +16,16 @@ function Header() {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="/account"><i className="far fa-user"></i>Account</Nav.Link>
-              <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+              <LinkContainer to="/account">
+                <Nav.Link href="/account">
+                  <i className="far fa-user"></i>Account
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/cart">
+                <Nav.Link href="/cart">
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
