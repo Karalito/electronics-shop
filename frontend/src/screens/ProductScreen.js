4547d997300ff4894e239dce41ce1ae631724bdc
Karalito <karl.pigaga@gmail.com>
2021-11-03 12:39:22 +0200

    Added Product details reducer, action and more
    Also added message and loader components.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,79 +1,85 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
-import axios from "axios";
+import {listProductDetails} from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+
 
 function ProductScreen({ match }) {
-  const [product, setProduct] = useState([]);
-  
+  const dispatch = useDispatch()
+  const productDetails = useSelector(state => state.productDetails)
+  const {loading, error, product} = productDetails
   useEffect(() => {
-    async function fetchProduct() {
-      const { data } = await axios.get(`/api/products/${match.params.id}`);
-      setProduct(data);
-    }
-
-    fetchProduct();
-  }, []);
+    dispatch(listProductDetails(match.params.id))
+  }, [dispatch, match]);
 
   return (
     <div>
       <Link to="/" className="btn btn-light my-3">
         Go Back
       </Link>
-      <Row>
-        <Col md={6} className="col-product-image">
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-              Price: {product.price}€
-            </ListGroup.Item>
+      {loading ? 
+        <Loader/>
+        : error
+          ? <Message variant='danger'>{error}</Message>
+        : (
+            <><Row>
+              <Col md={6} className="col-product-image">
+                <Image src={product.image} alt={product.name} fluid />
+              </Col>
+              <Col md={3}>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <h3>{product.name}</h3>
+                    Price: {product.price}€
+                  </ListGroup.Item>
 
-            <ListGroup.Item>Description : {product.description}</ListGroup.Item>
+                  <ListGroup.Item>Description: {product.description}</ListGroup.Item>
 
-            <ListGroup.Item>
-              <Row>
-                <Col>Status:</Col>
-                <Col>
-                  {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>
+                        <Button
+                          className="btn-block"
+                          disabled={product.countInStock == 0}
+                          type="button"
+                        >
+                          Add to Cart
+                        </Button>
+                      </Col>
+
+                      <Col>
+                        <Button
+                          className="btn-block"
+                          disabled={product.countInStock == 0}
+                          type="button"
+                        >
+                          Add to Wish List
+                        </Button>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Col>
+            </Row><Row>
+                <h3>Parameters</h3>
+                <Col md={3}>
+                  {product.parameters_id}
                 </Col>
-              </Row>
-            </ListGroup.Item>
-
-            <ListGroup.Item>
-              <Row>
-                <Col>
-                  <Button
-                    className="btn-block"
-                    disabled={product.countInStock == 0}
-                    type="button"
-                  >
-                    Add to Cart
-                  </Button>
-                </Col>
-
-                <Col>
-                  <Button
-                    className="btn-block"
-                    disabled={product.countInStock == 0}
-                    type="button"
-                  >
-                    Add to Wish List
-                  </Button>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
-      <Row>
-      <h3>Parameters</h3>
-      <Col md={3}>
-        {product.parameters_id}
-      </Col>
-      </Row>
+              </Row></>
+          )    
+      }
     </div>
   );
 }
