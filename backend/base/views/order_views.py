b5c030cd5c4e9404c8072cd21b148bf2698de37a
Karalito <karl.pigaga@gmail.com>
2021-11-12 14:25:27 +0200

    Added get order by id

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -23,34 +23,52 @@ def addOrderItems(request):
         return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
     else:
         order = Order.objects.create(
-            user = user,
-            paymentMethod = data['paymentMethod'],
-            shippingPrice = data['shippingPrice'],
-            totalPrice = data['totalPrice'],
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice'],
         )
 
         shipping = ShippingAddress.objects.create(
-            order = order,
-            country = data['shippingAddress']['country'],
-            city = data['shippingAddress']['city'],
-            address = data['shippingAddress']['address'],
-            postalCode = data['shippingAddress']['postalCode'],
-            
+            order=order,
+            country=data['shippingAddress']['country'],
+            city=data['shippingAddress']['city'],
+            address=data['shippingAddress']['address'],
+            postalCode=data['shippingAddress']['postalCode'],
+
         )
 
         for i in orderItems:
             product = Product.objects.get(_id=i['product'])
 
             item = OrderItem.objects.create(
-                product = product,
-                order = order,
-                name = product.name,
+                product=product,
+                order=order,
+                name=product.name,
                 qty=i['qty'],
-                price = i['price'],
-                image = product.image.url,
+                price=i['price'],
+                image=product.image.url,
             )
 
             product.count_in_stock -= item.qty
             product.save()
         serializer = OrderSerializer(order, many=False)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+
+    user = request.user
+
+    try:
+        order = Order.objects.get(_id=pk)
+        if(user.is_staff or order.user == user):
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail': 'Not authorized to view this order'},
+                     status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return(Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST))
